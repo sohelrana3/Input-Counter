c0f26dbd8bb2c7f56dc1582e7f64636e6613666a
Sohel Rana <expartdesigns5@gmail.com>
2023-05-15 11:04:14 +0600

    div create

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
   return (
     <section className='bg-cyan-400 h-screen'>
-      <div className='container mx-auto flex py-10'>
-        <div className='w-1/2'>
-          <di
+      <div className='container mx-auto flex py-10 h-screen'>
+        <div className='w-1/2 flex flex-col justify-between'>
+          <div>1</div>
+          <div>1</div>
+          <div>1dddddddddddd</div>
         </div>
         <div className='w-1/2'>12</div>
       </div>
